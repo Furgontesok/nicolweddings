77ab1588984b6db23101d6f19911571137d013c9
Furgontesok <furgontesok@users.noreply.github.com>
2026-06-29 21:08:50 +0200

    Rólam header kép pozíció még lejjebb (75%)

--- app/rolam/page.tsx
+++ app/rolam/page.tsx
@@ -21,7 +21,7 @@ export default function Rolam() {
             alt="Rólam"
             fill
             className="object-cover"
-            style={{ objectPosition: "center 50%" }}
+            style={{ objectPosition: "center 75%" }}
             sizes="100vw"
             priority
           />
