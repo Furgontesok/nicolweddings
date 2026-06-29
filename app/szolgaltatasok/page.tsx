99736fc365982fd1a1298641af1813c0eea96536
Furgontesok <furgontesok@users.noreply.github.com>
2026-06-29 19:43:47 +0200

    Szolgaltatasok header: mobilon 1 sor 25vh magassag

--- app/szolgaltatasok/page.tsx
+++ app/szolgaltatasok/page.tsx
@@ -283,7 +283,7 @@ export default function Szolgaltatasok() {
           </div>
 
           {/* Mobil: 1 sor */}
-          <div className="md:hidden" style={{ height: "50vh", minHeight: 320 }}>
+          <div className="md:hidden" style={{ height: "25vh", minHeight: 160 }}>
             <div className="hero-row-1 flex h-full">
               {[...heroImages, ...heroImages].map((src, i) => (
                 <div key={i} className="hero-img-wrap relative flex-shrink-0 h-full" style={{ width: "55vw" }}>
