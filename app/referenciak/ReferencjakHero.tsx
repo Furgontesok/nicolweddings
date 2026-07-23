41e9798d78d42480e94deaa6ced70911fec0cea9
Furgontesok <furgontesok@users.noreply.github.com>
2026-07-23 15:20:27 +0200

    Mobil: referenciák header fehér csík javítás

--- app/referenciak/ReferencjakHero.tsx
+++ app/referenciak/ReferencjakHero.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function ReferencjakHero() {
   return (
-    <div className="pt-[92px] bg-white">
+    <div className="pt-[66px] md:pt-[92px] bg-white">
       <div className="relative overflow-hidden h-[35vh] md:h-[50vh] lg:h-[55vh] 2xl:h-[62vh]" style={{ minHeight: 200 }}>
         <Image
           src="/images/referenciak-header.jpg"
